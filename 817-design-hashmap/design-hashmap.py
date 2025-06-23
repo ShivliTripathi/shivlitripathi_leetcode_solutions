6202c4db500490c7e0b6649eaf1fd999c6c067d7
ShivliTripathi <38250522+ShivliTripathi@users.noreply.github.com>
2025-06-23 08:30:04 -0400

    Time: 20 ms (94.92%) | Memory: 20.8 MB (96.20%) - LeetSync

--- 817-design-hashmap/design-hashmap.py
+++ 817-design-hashmap/design-hashmap.py
@@ -1,11 +1,10 @@
 class MyHashMap:
 
     def __init__(self):
-        
-        self.hashmap={}
+        self.hashmap = {}
 
     def put(self, key: int, value: int) -> None:
-        self.hashmap[key]=value
+        self.hashmap[key] = value
 
     def get(self, key: int) -> int:
         if key not in self.hashmap.keys():
