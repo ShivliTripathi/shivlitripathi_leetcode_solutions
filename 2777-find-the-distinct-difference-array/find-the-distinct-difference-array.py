043c812de1a31effe20704827f67b8c986731df8
ShivliTripathi <38250522+ShivliTripathi@users.noreply.github.com>
2025-07-10 07:54:22 -0400

    Time: 17 ms (52.49%) | Memory: 17.4 MB (100.00%) - LeetSync

---
+++ 2777-find-the-distinct-difference-array/find-the-distinct-difference-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def distinctDifferenceArray(self, nums: List[int]) -> List[int]:
+        diff=[]
+        for i in range(len(nums)):
+            prefix= len(set(nums[:i+1]))
+            suffix= len(set(nums[i+1:]))
+            diff.append(prefix-suffix)
+            
+        return diff
