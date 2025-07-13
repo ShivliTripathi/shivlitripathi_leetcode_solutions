6bd07cfa684078220e5bd8c4232400ae5f975e74
ShivliTripathi <38250522+ShivliTripathi@users.noreply.github.com>
2025-07-13 16:04:52 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (52.44%) - LeetSync

---
+++ 3396-valid-word/valid-word.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isValid(self, word: str) -> bool:
+
+        vowels = any(c for c in word if c.lower() in ['a','e','i','o','u'])
+        con = any(c for c in word if c.lower() not in ['a','e','i','o','u'] and c.isalpha())
+
+        if len(word) >= 3 and word.isalnum() and vowels and con:
+            return True
+        else:
+            return False 
