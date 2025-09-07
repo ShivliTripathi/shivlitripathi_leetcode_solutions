601cea2f57f9dc6694388eba5b02dfdada8e07cf
ShivliTripathi <38250522+ShivliTripathi@users.noreply.github.com>
2025-09-07 19:19:32 -0400

    Time: 278 ms (36.60%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -3,14 +3,16 @@ with cte as (select u.name as results
 from Users u join MovieRating mr
 on u.user_id=mr.user_id
 group by u.name
-order by count(mr.movie_id) desc, u.name limit 1),
-
-cte2 as (select m.title as results
-from Movies m join  MovieRating mr 
+order by count(mr.rating) desc, u.name
+limit 1),
+cte2 as
+(select m.title as results
+from Movies m join MovieRating mr
 on m.movie_id=mr.movie_id
-where created_at between '2020-02-01' and '2020-02-29'
+where to_char(mr.created_at,'YYYY-MM') = '2020-02'
 group by m.title
-order by avg(mr.rating) desc,m.title limit 1)
+order by avg(mr.rating) desc, m.title
+limit 1)
 
 select * from cte
 union all
