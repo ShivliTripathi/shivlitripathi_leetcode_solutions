76d33ada9705f4c4c189982b5ca959abed19197f
ShivliTripathi <38250522+ShivliTripathi@users.noreply.github.com>
2025-09-07 15:03:10 -0400

    Time: 387 ms (40.75%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,5 +1,6 @@
 -- Write your PostgreSQL query statement below
-select distinct v1.author_id as id 
+select distinct v1.author_id as id
 from Views v1 join Views v2
 on v1.article_id=v2.article_id
 where v1.author_id=v2.viewer_id
+group by v1.author_id
