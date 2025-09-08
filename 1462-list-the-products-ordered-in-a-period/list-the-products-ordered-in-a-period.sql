bcf64ca668c5e11153851e0af4b6201116589e81
ShivliTripathi <38250522+ShivliTripathi@users.noreply.github.com>
2025-09-07 20:08:18 -0400

    Time: 198 ms (98.35%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -1,8 +1,7 @@
 -- Write your PostgreSQL query statement below
-with cte as (select p.product_name, sum(o.unit) as unit
-from Products p join Orders o
+select p.product_name, sum(o.unit) as unit
+from Products p join Orders o 
 on p.product_id=o.product_id
-where o.order_date between '2020-02-01' and '2020-02-29'
-group by p.product_name)
-
-select * from cte where unit>=100
+where to_char(o.order_date,'yyyy-mm')='2020-02'
+group by p.product_name
+having sum(o.unit)>=100
