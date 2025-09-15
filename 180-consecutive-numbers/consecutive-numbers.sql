5af15ce61e3d6334eed131a907cc2690f2346d78
ShivliTripathi <38250522+ShivliTripathi@users.noreply.github.com>
2025-09-15 07:25:39 -0400

    Time: 332 ms (20.08%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,5 +1,6 @@
 -- Write your PostgreSQL query statement below
 select distinct l1.num as ConsecutiveNums
-from Logs l1
-join Logs l2 on l1.num=l2.num and l1.id+1 = l2.id
-join Logs l3 on l2.num=l3.num and l2.id+1 = l3.id
+from logs l1
+join logs l2 on l1.id+1=l2.id
+join logs l3 on l2.id+1=l3.id
+where l1.num=l2.num and l2.num=l3.num
