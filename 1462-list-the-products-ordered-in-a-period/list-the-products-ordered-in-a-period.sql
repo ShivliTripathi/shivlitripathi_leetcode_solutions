e8fd1a38757ae12920fce1d474ec4847e020b087
ShivliTripathi <38250522+ShivliTripathi@users.noreply.github.com>
2026-08-28 14:53:02 -0400

    Time: 803 ms (54.96%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -1,7 +1,8 @@
--- Write your PostgreSQL query statement below
-select p.product_name, sum(o.unit) as unit
-from Products p join Orders o 
+# Write your MySQL query statement below
+select p.product_name,sum(o.unit) as unit  from Products p
+join Orders o
 on p.product_id=o.product_id
-where to_char(o.order_date,'yyyy-mm')='2020-02'
-group by p.product_name
-having sum(o.unit)>=100
+where o.order_date >='2020-02-01' and o.order_date <='2020-02-29'
+group by o.product_id
+having sum(unit)>=100
+
